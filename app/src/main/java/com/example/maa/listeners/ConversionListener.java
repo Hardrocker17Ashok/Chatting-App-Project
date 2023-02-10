892a46eb1d9c339e964f77859aa8ac54c8405b70
macbookpro <ashoksharmaas9358@gmail.com>
2023-02-10 13:42:30 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/maa/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.maa.listeners;
+
+import com.example.maa.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/maa/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.maa.listeners;
+
+import com.example.maa.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
